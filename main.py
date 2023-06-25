0af7d1df1644a5a0ff9d669d90673cec2f714844
torrent 2 <torrent2@users.noreply.huggingface.co>
2023-06-25 15:35:14 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 
 os.system('curl -OJ "https://codeload.github.com/novnc/noVNC/zip/refs/tags/v1.4.0"')
-os.system('unzip "./noVNC-1.4.0.zip" &> /dev/null')
+os.system('unzip "./noVNC-1.4.0.zip"')
 
 os.system('echo $PATH')
 os.system('echo $PWD')
@@ -10,6 +10,6 @@ os.system('echo -e "123456\n123456\nn\n" | vncpasswd')
 os.system('echo -e "#!/bin/sh\nxfce4-terminal" > ~/.vnc/xstartup')
 
 os.system('vncserver -depth 32 -geometry 1200x900 &')
-os.system('noVNC-1.4.0/utils/novnc_proxy --vnc localhost:5901 --listen localhost:7860 &>/dev/null &')
+os.system('./noVNC-1.4.0/utils/novnc_proxy --vnc localhost:5901 --listen localhost:7860 &>/dev/null &')
 
 #os.system("python -m http.server 7860")
