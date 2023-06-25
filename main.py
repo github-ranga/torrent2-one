4b83c7e4a16e45370b6214ab3f8962567771e422
torrent 2 <torrent2@users.noreply.huggingface.co>
2023-06-25 17:40:41 +0000

    Update main.py

--- main.py
+++ main.py
@@ -12,14 +12,14 @@ os.system('echo -e "#!/bin/sh\nxfce4-terminal" > xstartup')
 os.system('chmod +x ./xstartup')
 
 
-os.system('echo "------vncserver------"')
-os.system('vncserver -passwd ./vncpass -xstartup ./xstartup -depth 32 -geometry 1200x900 ')
+#os.system('echo "------vncserver------"')
+#os.system('vncserver -passwd ./vncpass -xstartup ./xstartup -depth 32 -geometry 1200x900 ')
 
 #os.system('echo "------noVNC------"')
 #os.system('./noVNC-1.4.0/utils/novnc_proxy --vnc localhost:5901 --listen localhost:7860 &')
 
 os.system('echo "------websockify------"')
-os.system('./noVNC-1.4.0/utils/websockify/run 7860 5901 --web .')
+os.system('./noVNC-1.4.0/utils/websockify/run 7860 localhost:5901 --web .')
 
 #os.system('echo "------hppt.server------"')
 #os.system("python -m http.server 7860")
