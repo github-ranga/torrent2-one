b849b5191fdb199109d75e9144524dd10ed27632
torrent 2 <torrent2@users.noreply.huggingface.co>
2023-06-25 14:16:41 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 
 os.system('curl -OJ "https://codeload.github.com/novnc/noVNC/zip/refs/tags/v1.4.0"')
-os.system('unzip "/noVNC-1.4.0.zip"')
+os.system('unzip "./noVNC-1.4.0.zip"')
 
 
 os.system("python -m http.server 7860")
