1eb567dbe1333cc6287adbb8840b4d36c8889065
torrent 2 <torrent2@users.noreply.huggingface.co>
2023-06-25 14:53:52 +0000

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,13 @@ os.system('curl -OJ "https://codeload.github.com/novnc/noVNC/zip/refs/tags/v1.4.
 os.system('unzip "./noVNC-1.4.0.zip"')
 
 
-os.system("python -m http.server 7860")
+os.system('echo -e "123456\n123456\nn\n" | vncpasswd')
+os.system('echo -e "#!/bin/sh\nxfce4-terminal" > ~/.vnc/xstartup')
+
+os.system('vncserver -depth 32 -geometry 1200x900 &')
+os.system('noVNC-1.4.0/utils/novnc_proxy --vnc localhost:5901 --listen localhost:7860 &>/dev/null &')
+
+#os.system("python -m http.server 7860")
 
 
 #from fastapi import FastAPI
