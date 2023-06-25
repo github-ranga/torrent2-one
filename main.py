cceff423aace2bb07e88418bff27d8f928d4e6c0
torrent 2 <torrent2@users.noreply.huggingface.co>
2023-06-25 16:34:44 +0000

    Update main.py

--- main.py
+++ main.py
@@ -6,19 +6,12 @@ os.system('echo $PATH')
 os.system('echo $PWD')
 
 os.system('echo -e "123456" > vncpass')
-os.system('mkdir "~/.vnc"')
-os.system('vncpasswd -f < vncpass > ~/.vnc/passwd')
-os.system('echo -e "#!/bin/sh\nxfce4-terminal" > ~/.vnc/xstartup')
+os.system('echo -e "#!/bin/sh\nxfce4-terminal" > xstartup')
 
-os.system('echo "------ls ~/.vnc------"')
-os.system('ls ~/.vnc')
-os.system('echo "------cat ~/.vnc/xstartup------"')
-os.system('ls ~/.vnc/xstartup')
-os.system('echo "------cat ~/.vnc/passwd------"')
-os.system('ls ~/.vnc/passwd')
 
 os.system('echo "------vncserver------"')
-os.system('vncserver -depth 32 -geometry 1200x900 ')
-#os.system('./noVNC-1.4.0/utils/novnc_proxy --vnc localhost:5901 --listen localhost:7860 &>/dev/null &')
+os.system('chmod +x ./xstartup')
+os.system('vncserver -passwd ./vncpass -xstartup ./xstartup -depth 32 -geometry 1200x900 ')
+os.system('./noVNC-1.4.0/utils/novnc_proxy --vnc localhost:5901 --listen localhost:7860 &>/dev/null &')
 
 #os.system("python -m http.server 7860")
