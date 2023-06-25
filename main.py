504aa3af9e7988e2fb853e060a1975bff919e96c
torrent 2 <torrent2@users.noreply.huggingface.co>
2023-06-25 17:16:57 +0000

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ os.system('echo "------vncserver------"')
 os.system('vncserver -passwd ./vncpass -xstartup ./xstartup -depth 32 -geometry 1200x900 ')
 
 os.system('echo "------noVNC------"')
-os.system('./noVNC-1.4.0/utils/novnc_proxy --vnc localhost:5901 --listen localhost:7860 --web &')
+os.system('./noVNC-1.4.0/utils/novnc_proxy --vnc localhost:5901 --listen localhost:7860 --web ')
 
 
 #os.system('echo "------hppt.server------"')
