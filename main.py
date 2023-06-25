359db5de3fb3549e8ac5df57d3cd40e5f5c85aec
torrent 2 <torrent2@users.noreply.huggingface.co>
2023-06-25 17:13:39 +0000

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import os
 
 os.system('curl -OJ "https://codeload.github.com/novnc/noVNC/zip/refs/tags/v1.4.0" > /dev/null')
 os.system('unzip "./noVNC-1.4.0.zip" > /dev/null')
+os.system('git clone https://github.com/novnc/websockify ./noVNC-1.4.0/utils/websockify')
+
 os.system('echo $PATH')
 os.system('echo $PWD')
 
@@ -14,7 +16,7 @@ os.system('echo "------vncserver------"')
 os.system('vncserver -passwd ./vncpass -xstartup ./xstartup -depth 32 -geometry 1200x900 ')
 
 os.system('echo "------noVNC------"')
-os.system('./noVNC-1.4.0/utils/novnc_proxy --vnc localhost:5901 --listen localhost:7860 --web')
+os.system('./noVNC-1.4.0/utils/novnc_proxy --vnc localhost:5901 --listen localhost:7860 --web &')
 
 
 os.system('echo "------hppt.server------"')
