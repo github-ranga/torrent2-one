c31c325fc4742d3ec283be32939129d5367d01b9
torrent 2 <torrent2@users.noreply.huggingface.co>
2023-06-25 16:47:24 +0000

    Update main.py

--- main.py
+++ main.py
@@ -11,8 +11,8 @@ os.system('echo -e "#!/bin/sh\nxfce4-terminal" > xstartup')
 
 os.system('echo "------vncserver------"')
 os.system('chmod +x ./xstartup')
-os.system('vncserver -passwd ./vncpass -xstartup ./xstartup -depth 32 -geometry 1200x900 &')
+os.system('vncserver -passwd ./vncpass -xstartup ./xstartup -depth 32 -geometry 1200x900 ')
 os.system('echo "------noVNC------"')
-os.system('./noVNC-1.4.0/utils/novnc_proxy --vnc localhost:5901 --listen localhost:7860 &')
+os.system('./noVNC-1.4.0/utils/novnc_proxy --vnc localhost:5901 --listen localhost:7860 --web ')
 
 #os.system("python -m http.server 7860")
