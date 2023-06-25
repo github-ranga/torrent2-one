42b0b9761cfd3f03c0aaed8392549ac05c6eb7b1
torrent 2 <torrent2@users.noreply.huggingface.co>
2023-06-25 19:19:34 +0000

    Update main.py

--- main.py
+++ main.py
@@ -3,12 +3,8 @@ import os
 os.system('curl -OJ "https://codeload.github.com/novnc/noVNC/zip/refs/tags/v1.4.0" > /dev/null')
 os.system('unzip "./noVNC-1.4.0.zip" > /dev/null')
 os.system('git clone https://github.com/novnc/websockify ./noVNC-1.4.0/utils/websockify')
-os.chdir('/home/user/app')
-os.system('cd /home/user/app')
-os.system('echo $PATH')
-os.system('echo $PWD')
 
-os.system('chmod +x ./xstartup')
+os.system('chmod +x /home/user/app/xstartup')
 
 
 
@@ -16,11 +12,11 @@ os.system('chmod +x ./xstartup')
 #os.system('./noVNC-1.4.0/utils/novnc_proxy --vnc localhost:5901 --listen localhost:7860 &')
 
 os.system('echo "------websockify------"')
-os.system('./noVNC-1.4.0/utils/websockify/run --token-plugin=TokenFile --token-source=./tok 0.0.0.0:7860 --web=./ ')
+os.system('./noVNC-1.4.0/utils/websockify/run --token-plugin=TokenFile --token-source=/home/user/app/tok 0.0.0.0:7860 --web=/home/user/app ')
 
 
 #os.system('echo "------vncserver------"')
-#os.system('vncserver -passwd ./vncpass -xstartup ./xstartup -depth 32 -geometry 1200x900 ')
+#os.system('vncserver -passwd /home/user/app/vncpass -xstartup /home/user/app/xstartup -depth 32 -geometry 1200x900 ')
 
 
 
